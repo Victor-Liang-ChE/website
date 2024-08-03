0677b91e831221f95de171df7e174f59dd9ff75f
Victor Liang <112746859+Victor-Liang-ChE@users.noreply.github.com>
2024-08-03 16:16:14 -0700

    Add files via upload

---
+++ mywebsite.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Chemical Engineering Graphing", page_icon=":video_game:", layout="wide") 
+
+st.subheader("Chemical Engineering Graphing")
+st.title("Welcome to the Chemical Engineering Graphing Website")
+st.write("This website is designed to help chemical engineering students visualize and understand the concepts of chemical engineering through interactive graphs and charts.")
+st.write("This website is a work in progress and will be updated with new features and content regularly. If you have any suggestions or feedback, please feel free to reach out to me at victorliang@ucsb.edu")
+
+
