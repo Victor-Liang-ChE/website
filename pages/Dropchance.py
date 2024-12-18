9c8218948ed6e919c555fe3f38ee6f80174a637c
Victor-Liang-ChE <112746859+Victor-Liang-ChE@users.noreply.github.com>
2024-12-18 00:06:19 -0800

    added a favicon, drop chance now clientside

--- pages/Dropchance.py
+++ pages/Dropchance.py
@@ -1,13 +1,7 @@
-from dash import html, dcc, callback, Output, Input
+from dash import html, dcc, clientside_callback, Output, Input
 import dash
 
 dash.register_page(__name__, path='/dropchance', name="Drop Chance Calculator")
-
-def dropchance(percent, attempts):
-    if attempts == 1:
-        return f'There is a {100 - ((100 - percent) / 100) ** attempts * 100:.1f}% chance that you will receive the {percent}% drop at least once in {attempts} try.'
-    else:
-        return f'There is a {100 - ((100 - percent) / 100) ** attempts * 100:.1f}% chance that you will receive the {percent}% drop at least once in {attempts} tries.'
 
 layout = html.Div([
     html.Div([
@@ -39,15 +33,24 @@ layout = html.Div([
     html.Div(id='result-output', style={'margin-top': '20px', 'font-size': '20px', 'margin-left': '20px', 'margin-right': '20px'})
 ])
 
-@callback(
-    Output('percent-display', 'children'),
-    Output('attempts-display', 'children'),
-    Output('result-output', 'children'),
-    Input('percent-slider', 'value'),
-    Input('attempts-slider', 'value')
+clientside_callback(
+    """
+    function(percent, attempts) {
+        const percent_display = `${percent}%`;
+        const attempts_display = `${attempts}`;
+        let result;
+        const chance = 100 - Math.pow((100 - percent) / 100, attempts) * 100;
+        if (attempts == 1) {
+            result = `There is a ${chance.toFixed(1)}% chance that you will receive the ${percent}% drop at least once in ${attempts} try.`;
+        } else {
+            result = `There is a ${chance.toFixed(1)}% chance that you will receive the ${percent}% drop at least once in ${attempts} tries.`;
+        }
+        return [percent_display, attempts_display, result];
+    }
+    """,
+    [Output('percent-display', 'children'),
+     Output('attempts-display', 'children'),
+     Output('result-output', 'children')],
+    [Input('percent-slider', 'value'),
+     Input('attempts-slider', 'value')]
 )
-def update_output(percent, attempts):
-    percent_display = f'{int(percent)}%'
-    attempts_display = str(attempts)
-    result = dropchance(percent, attempts)
-    return percent_display, attempts_display, result
